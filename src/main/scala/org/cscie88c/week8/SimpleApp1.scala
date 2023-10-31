78b76613ebda9cb8bdc1e01f0852d1066178d8fc
sankalp singh <sankalpsingh@sankalps-MacBook-Pro.local>
2023-10-31 16:37:14 -0700

    week8

--- src/main/scala/org/cscie88c/week8/SimpleApp1.scala
+++ src/main/scala/org/cscie88c/week8/SimpleApp1.scala
@@ -12,8 +12,26 @@ object SimpleApp1 extends LazyLogging {
       LazyList.from(Source.fromResource(fileName).getLines())
     }.toOption
   
-  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] = ???
+  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] = {
+      lines.filter(x  => 
+        try {
+          x.split(",").apply(1).split("-").apply(1) == month
+      } catch {
+        case _  => false
+      }
+      )
+  }
       
-  def main(args: Array[String]): Unit = ???
+  def main(args: Array[String]): Unit = {
+    val fileName = args(0)
+    val month = args(1)
+
+    val lines = lineStreamFromFile(fileName)
+
+    lines match {
+      case None => println(s"No Transactions found for ${month}")
+      case Some(value) => println(s"Transactions in ${month}: ${monthLines(month)(value).size}")
+    }
+  }
 
 }
--- src/main/scala/org/cscie88c/week8/SimpleApp2.scala
+++ src/main/scala/org/cscie88c/week8/SimpleApp2.scala
@@ -8,8 +8,12 @@ import pureconfig.generic.auto._
 
 // write case class here
 
+case class SimpleApp2Config(fileName: String, month: String)
+
 // run with: sbt "runMain org.cscie88c.week8.SimpleApp2"
 object SimpleApp2 extends LazyLogging{
+
+  val CONF_PATH = "org.cscie88c.simple-app-2"
   
 
   def lineStreamFromFile(fileName: String): Option[LazyList[String]] =
@@ -17,7 +21,27 @@ object SimpleApp2 extends LazyLogging{
       LazyList.from(Source.fromResource(fileName).getLines())
     }.toOption
   
-  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] = ???
-      
-  def main(args: Array[String]): Unit = ???
+  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] = {
+      lines.filter(x  => 
+        try {
+          x.split(",").apply(1).split("-").apply(1) == month
+      } catch {
+        case _  => false
+      }
+      )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val conf = ConfigUtils.loadAppConfig[SimpleApp2Config](CONF_PATH)
+    
+    val fileName = conf.fileName
+    val month = conf.month
+
+    val lines = lineStreamFromFile(fileName)
+
+    lines match {
+     case None => println(s"No Transactions found for ${month}")
+     case Some(value) => println(s"Transactions in ${month}: ${monthLines(month)(value).size}")
+    }
+  }
 }
